3e0ac444b0d34c0f1a34e896ce079517d8149e35
Soumyadeepaul <92711266+Soumyadeepaul@users.noreply.github.com>
2022-11-12 22:12:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask,render_template
 app=Flask(__name__)
 @app.route('/')
 def hi():
-    return "hi"
+    return render_template('try.html')
 if __name__=='__main__':
     app.run(debug=True)
